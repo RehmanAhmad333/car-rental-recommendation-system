3e12b2d923f957d1ee978712765fd298ea1b3c0b
Rehman Ahmad <rahmanahmadcheema07@gmail.com>
2026-03-31 17:01:18 +0500

    Update data.py file

--- Day_8/data.py
+++ Day_8/data.py
@@ -15,22 +15,24 @@ uri = os.getenv("MONGODB_URI")
 print("Using URI:", uri.replace(uri.split('@')[0].split(':')[1], '****'))  # hide password
 
 try:
-    client = MongoClient(uri)
-    # client.admin.command('ping')
+    client = MongoClient(uri, serverSelectionTimeoutMS=5000)
+    client.admin.command('ping')
     print("Connection successful!")
+
+    db = client["test"]
+    collection = db["CarSchema"]
+
 except Exception as e:
     print("Connection failed:", e)
-
-# db = client[""]          # DB name
-# collection = db[""]  # collection name
-
-# all_docs = collection.find({})
-# for doc in all_docs:
-#     print(doc)
+    db = None
+    collection = None
 
 
 
 @app.get("/data")
 def datafile():
-    return {"message": "Hello"}
+    if collection is None:
+        return {"error": "Database not connected"}
     
+    all_docs = list(collection.find({}, {"_id": 0}))
+    return {"data": all_docs}
